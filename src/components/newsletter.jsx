e03f3fa837f62630e84cf18113a4e46404f86f51
Riade-bg <riadeboughaba@gmail.com>
2022-09-23 20:41:07 +0100

    update

--- src/components/newsletter.jsx
+++ src/components/newsletter.jsx
@@ -11,12 +11,12 @@ const News = () => {
             {!formSubmit ? (
                 <>
                  <div className="relative flex justify-center w-full lg:w-9/12 mx-auto">
-                     <input type="email" className="text-white w-full px-10 py-5 bg-gray-700 mx-auto rounded-md" placeholder="Type Your E-mail" />
+                     <input type="email" className="text-white w-full px-10 py-5 bg-gray-700 mx-auto rounded-md" placeholder="E-mail..." />
                      <button onClick={() => setFormSubmit(true)} className="bg-violet-800 hover:bg-violet-800/80 font-main font-bold text-white px-10 py-5 rounded-md absolute right-0">Subscribe</button>
                  </div>
                  </>
             ) : (
-                <h1 className="bg-green-100 text-green-900 w-96 mx-auto text-center font-bold rounded-md py-4">
+                <h1 className="bg-green-100 text-green-900 w-full lg:w-96 mx-auto text-center font-bold rounded-md py-4">
                     Done!
                 </h1>
             )}
